6c6e3e90628ce016e3da33be3a1f92fe764ce011
kadirtereci <kadir@oakslab.com>
2022-10-12 11:33:48 +0300

    text change

--- server.ts
+++ server.ts
@@ -48,7 +48,7 @@ app.use(
 );
 
 app.all("/", (req, res) => {
-  res.send("This route is only for 3rd party services-1");
+  res.send("This route is only for 3rd party services-");
 });
 
 const PORT = process.env.PORT || 8080;
